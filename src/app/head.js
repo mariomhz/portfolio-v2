2e6c824b0dcde3e1fcd3c48f54cfeeae5fb51de0
m4ri000000 <josemarioahernandez@gmail.com>
2025-10-21 20:56:52 +0100

    Added head.js

---
+++ src/app/head.js
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <title>Mario Hernández – Front-end Craftsman</title>
+      <meta name="description" content="Portfolio of Mario Hernández, front-end developer with UX/UI focus." />
+    </>
+  );
+}
