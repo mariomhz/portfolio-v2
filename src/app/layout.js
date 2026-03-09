fcda28dd5d34ca46d242d66802d00374b0fcd214
mariomhz <mariomaartin8@gmail.com>
2026-03-09 08:59:27 +0000

    docs: updated name in openGraph

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,7 @@ export const metadata = {
   title: "MÁRIO HERNÁNDEZ \ FRONTEND & FULLSTACK DEVELOPER",
   description: "Frontend & fullstack developer specializing in React, Next.js, and Spring Boot. Based in Europe.",
   openGraph: {
-    title: "MARIO HERNÁNDEZ || FRONTEND & FULLSTACK DEVELOPER",
+    title: "MÁRIO HERNÁNDEZ \ FRONTEND & FULLSTACK DEVELOPER",
     description: "Frontend & fullstack developer specializing in React, Next.js, and Spring Boot. Based in Europe.",
     type: "website",
   },
